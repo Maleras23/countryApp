3bc5578d43e88fe0a707995c40f948e0f19de06e
Carlos Calzadilla <maleras24@gmail.com>
2025-11-18 14:36:37 +0000

    Countries Services Modified

--- src/app/countries/countries.module.ts
+++ src/app/countries/countries.module.ts
@@ -7,6 +7,7 @@ import { ByRegionPageComponent } from './pages/by-region-page/by-region-page.com
 import { CountryPageComponent } from './pages/country-page/country-page.component';
 import { CountriesRoutingModule } from './countries-routing.module';
 import { SharedModule } from "../shared/shared.module";
+import { CountryTableComponent } from './components/countryTable/countryTable.component';
 
 
 
@@ -15,12 +16,13 @@ import { SharedModule } from "../shared/shared.module";
     ByCapitalPageComponent,
     ByCountryPageComponent,
     ByRegionPageComponent,
-    CountryPageComponent
+    CountryPageComponent,
+    CountryTableComponent,
   ],
   imports: [
     CommonModule,
     CountriesRoutingModule,
     SharedModule,
-  ]
+]
 })
 export class CountriesModule { }
