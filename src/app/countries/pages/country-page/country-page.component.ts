e6d15479cd39de04ea363b9d092e0c8d8dd69246
Carlos Calzadilla <maleras24@gmail.com>
2025-11-19 15:22:09 +0000

    Country page component added and working

--- src/app/countries/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CountriesService } from '../../services/countries.service';
+import { switchMap } from 'rxjs';
+import { Country } from '../../interfaces/country';
 
 @Component({
   selector: 'countries-country-page',
@@ -6,6 +10,33 @@ import { Component } from '@angular/core';
   templateUrl: './country-page.component.html',
   styles: ``,
 })
-export class CountryPageComponent {
+export class CountryPageComponent implements OnInit {
+
+  public country?: Country;
+
+  constructor(
+    private activatedRouted: ActivatedRoute,
+    private countriesService: CountriesService,
+    private router: Router
+  ){}
+
+
+  ngOnInit(): void {
+    this.activatedRouted.params
+      .pipe(
+        switchMap( ({ id }) => this.countriesService.searchCountryByAlphaCode(id)),
+      )
+      .subscribe( country => {
+        if (!country) return  this.router.navigateByUrl('');
+        return this.country = country;
+      })
+  }
+
+  // searchCountry( code: string){
+  //   this.countriesService.searchCountryByAlphaCode(code)
+  //     .subscribe(country => {
+  //         console.log({country})
+  //       });
+  // }
 
 }
--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,19 +1,25 @@
 
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { catchError, Observable, of } from 'rxjs';
+import { catchError, map, Observable, of } from 'rxjs';
 import { Country } from '../interfaces/country';
 
 @Injectable({providedIn: 'root'})
 export class CountriesService {
 
-  // base del url que usamos para hacer la llamada http
   private apiUrl: string = 'https://restcountries.com/v3.1'
 
-  // aqui estamos inportando el httpClient para poder hacer las llamadas y tambien lo puedes importar en la app.module
   constructor(private http: HttpClient) { }
 
-  // este es el metodo que hace la llamada http y retornar un observable que se importa de rxjs
+  searchCountryByAlphaCode(code: string):Observable<Country | null >{
+    const url = `${this.apiUrl}/alpha/${code}`;
+    return this.http.get<Country[]>(url)
+      .pipe(
+        map( countries => countries.length > 0 ? countries[0]: null),
+        catchError( () => of(null) )
+      );
+  }
+
   searchCapital(term: string): Observable<Country[]>{
     const url = `${this.apiUrl}/capital/${term}`;
     return this.http.get<Country[]>(url)
